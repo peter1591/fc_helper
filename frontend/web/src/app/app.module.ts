1e487caadf8ce33c4a55119d8f9aaa16992b6e4a
Shen-En Shih <petershih@google.com>
2023-02-19 13:21:55 -0800

    add pwa: ng add @angular/pwa --project web

--- frontend/web/src/app/app.module.ts
+++ frontend/web/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {GrpcCoreModule} from '@ngx-grpc/core';
 import {GrpcWebClientModule} from '@ngx-grpc/grpc-web-client';
 
 import {AppComponent} from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations : [ AppComponent ],
@@ -15,6 +16,12 @@ import {AppComponent} from './app.component';
     GrpcCoreModule.forRoot(),
     GrpcWebClientModule.forRoot({
       settings : {host : 'http://192.168.1.10:5782'},
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers : [],
